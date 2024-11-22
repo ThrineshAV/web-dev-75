2288a94155a64a7723c4c40c052b14101eb9f7c1
ThrineshAV <avthrinesh@gmail.com>
2024-11-22 16:53:55 +0530

    api

--- TeenFin/api/apps.py
+++ TeenFin/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TeenfinappConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teenfinapp'
+    name = 'api'
