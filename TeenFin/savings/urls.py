0cf1276840fa3da478504bb812d863297a98bb18
Sujan-coder-sudo <coolsujanr27@gmail.com>
2024-11-29 17:35:01 +0530

    Savings api

--- TeenFin/savings/urls.py
+++ TeenFin/savings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import MonthListCreateView, MonthRetrieveUpdateDeleteView
 
 urlpatterns = [
-    path('api/months/', MonthListCreateView.as_view(), name='month-list-create'),
-    path('api/months/<int:pk>/', MonthRetrieveUpdateDeleteView.as_view(), name='month-retrieve-update-delete'),
+    path('months/', MonthListCreateView.as_view(), name='month-list-create'),
+    path('months/<int:pk>/', MonthRetrieveUpdateDeleteView.as_view(), name='month-retrieve-update-delete'),
 ]
