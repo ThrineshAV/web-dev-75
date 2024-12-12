2be7e85cd8397dd74cdab2dd98696b3a45aad866
Sujan-coder-sudo <coolsujanr27@gmail.com>
2024-12-12 18:27:15 +0530

    Modifications

--- TeenFin/savings/models.py
+++ TeenFin/savings/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from teenfin import settings
 from rest_framework.authtoken.models import Token
+from datetime import datetime
+
 
 class Month(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    Month_name = models.CharField
-    Expense = models.IntegerField
 
+    Month_name = models.CharField(max_length=200, default ="")
+    Expense = models.IntegerField
+    Year = models.IntegerField(default=datetime.now().year)
     def __str__(self):
         return "Month Data Record"
 class Set_Goal(models.Model):
