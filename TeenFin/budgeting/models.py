c0dac4495c47ff4ce27ec1e72dc0292935082eba
ThrineshAV <avthrinesh@gmail.com>
2024-12-16 22:18:14 +0530

    budegeting working

--- TeenFin/budgeting/models.py
+++ TeenFin/budgeting/models.py
@@ -1,37 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
-from teenfin import settings
-from rest_framework.authtoken.models import Token
-
-class Category(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    color = models.CharField(max_length=7, help_text="Hex color code, e.g., #FF6384")
-    icon = models.CharField(max_length=50, help_text="Icon name (from frontend library)")
-
-    def __str__(self):
-        return self.name
-
-class Income(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Ensure there's a default
-
-class Savings(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Ensure there's a default
-
 
 class Expense(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Travel', 'Travel'),
+        ('Entertainment', 'Entertainment'),
+        ('Utilities', 'Utilities'),
+        ('Other', 'Other'),
+    ]
+
+    title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateField(auto_now_add=True)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='Other')
+    
     
 
-
     def __str__(self):
-        return f"{self.name} - {self.amount}"
+        return f"{self.title} - {self.amount}"
